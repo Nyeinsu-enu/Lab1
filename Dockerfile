3cf9d51c92b7e84c3179fe6f26c2ebeb81fcde12
Nyeinsu-enu <40804619@live.napier.ac.uk>
2025-09-23 21:20:30 +0630

    Updating Our System

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM openjdk:24-jdk
-COPY ./target/classes /tmp/classes
-WORKDIR /tmp/classes
-ENTRYPOINT ["java", "com.nepier.App"]
+#COPY ./target/classes /tmp/classes
+#WORKDIR /tmp/classes
+#ENTRYPOINT ["java", "com.nepier.App"]
+
+#FROM openjdk:latest
+COPY ./target/Lab_1-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
+WORKDIR /tmp
+ENTRYPOINT ["java", "-jar", "Lab_1-1.0-SNAPSHOT-jar-with-dependencies.jar"]
 
 
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.mongodb.MongoClient;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,6 +15,11 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
     }
+
 }
+
 
